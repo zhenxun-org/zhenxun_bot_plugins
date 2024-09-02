4a34e47fe6f71f746028529cc6f0784b9a85432a
xuaner <xuaner_wa@qq.com>
2024-09-02 23:18:32 +0800

    Feat: update `bilibili_sub` 0.2

---
+++ plugins/bilibili_sub/__init__.py
@@ -0,0 +1,376 @@
+from typing import Optional
+from arclet.alconna.typing import CommandMeta
+import nonebot
+from nonebot.plugin import PluginMetadata
+from nonebot import Driver
+from nonebot_plugin_session import EventSession
+from nonebot_plugin_alconna import Alconna, Args, on_alconna, UniMessage
+from nonebot.adapters.onebot.v11 import Bot
+from nonebot_plugin_apscheduler import scheduler
+from nonebot.params import ArgStr
+from nonebot.typing import T_State
+from nonebot.log import logger
+from nonebot.permission import SUPERUSER
+
+from nonebot.matcher import Matcher
+from bilireq.login import Login
+from zhenxun.utils.platform import PlatformUtils
+
+from zhenxun.configs.config import Config
+from zhenxun.services.log import logger  # noqa: F811
+from zhenxun.utils.image_utils import text2image
+from zhenxun.utils.message import MessageUtils
+from zhenxun.configs.utils import PluginExtraData, RegisterConfig
+from zhenxun.models.group_console import GroupConsole
+
+from .data_source import (
+    BilibiliSub,
+    SubManager,
+    add_live_sub,
+    add_season_sub,
+    add_up_sub,
+    delete_sub,  # noqa: F401
+    get_media_id,
+    get_sub_status,
+)
+
+import time
+from io import BytesIO
+
+from .auth import AuthManager
+from .utils import calc_time_total
+
+base_config = Config.get("bilibili_sub")
+
+__plugin_meta__ = PluginMetadata(
+    name="B站订阅",
+    description="非常便利的B站订阅通知",
+    usage="""
+        usage：
+            B站直播，番剧，UP动态开播等提醒
+            主播订阅相当于 直播间订阅 + UP订阅
+            指令：
+                添加订阅 ['主播'/'UP'/'番剧'] [id/链接/番名]
+                删除订阅 ['主播'/'UP'/'id'] [id]
+                查看订阅
+            示例：   
+                添加订阅主播 2345344 <-(直播房间id)
+                添加订阅UP 2355543 <-(个人主页id)
+                添加订阅番剧 史莱姆 <-(支持模糊搜索)
+                添加订阅番剧 125344 <-(番剧id)
+                删除订阅id 2324344 <-(任意id，通过查看订阅获取)
+        """.strip(),
+    extra=PluginExtraData(
+        author="HibiKier",
+        version="0.2",
+        superuser_help="""
+    登录b站获取cookie防止风控：
+            bil_check/检测b站
+            bil_login/登录b站
+            bil_logout/退出b站 uid
+            示例:
+                登录b站 
+                检测b站
+                bil_logout 12345<-(退出登录的b站uid，通过检测b站获取)
+        """,
+        configs=[
+            RegisterConfig(
+                module="bilibili_sub",
+                key="LIVE_MSG_AT_ALL",
+                value=False,
+                help="直播提醒是否AT全体（仅在真寻是管理员时生效）",
+                default_value=False,
+                type=bool
+
+            ),
+            RegisterConfig(
+                module="bilibili_sub",
+                key="UP_MSG_AT_ALL",
+                value=False,
+                help="UP动态投稿提醒是否AT全体（仅在真寻是管理员时生效）",
+                default_value=False,
+                type=bool,
+            ),
+            RegisterConfig(
+                module="bilibili_sub",
+                key="CHECK_TIME",
+                value=60,
+                help="b站检测时间间隔(秒)",
+                default_value=60,
+                type=int,
+            )
+        ],
+        admin_level=base_config.get("GROUP_BILIBILI_SUB_LEVEL"),
+    ).dict(),
+)
+
+Config.add_plugin_config(
+    "bilibili_sub",
+    "GROUP_BILIBILI_SUB_LEVEL",
+    5,
+    help="群内bilibili订阅需要管理的权限",
+    default_value=5,
+    type=int,
+)
+
+add_sub = on_alconna(Alconna("添加订阅", Args["sub_type", str]["sub_msg", str], meta=CommandMeta(compact=True)),
+                     aliases={"d", "添加", "添加订阅"},
+                     priority=5, block=True)
+del_sub = on_alconna(Alconna("删除订阅", Args["sub_type", str]["sub_msg", str], meta=CommandMeta(compact=True)),
+                     aliases={"td", "删除", "取消订阅"},
+                     priority=5, block=True)
+show_sub_info = on_alconna("查看订阅", priority=5, block=True)
+
+blive_check = on_alconna(Alconna("bil_check"), aliases={"检测b站", "检测b站登录", "b站登录检测"}, permission=SUPERUSER,
+                         priority=5,
+                         block=True)
+blive_login = on_alconna(Alconna("bil_login"), aliases={"登录b站", "b站登录"}, permission=SUPERUSER,
+                         priority=5,
+                         block=True)
+blive_logout = on_alconna(
+    Alconna("bil_logout", Args["uid", int]), aliases={"退出b站", "退出b站登录", "b站登录退出"}, permission=SUPERUSER,
+    priority=5,
+    block=True, )
+
+driver: Driver = nonebot.get_driver()
+
+sub_manager: Optional[SubManager] = None
+
+
+@driver.on_startup
+async def _():
+    global sub_manager
+    sub_manager = SubManager()
+
+
+@add_sub.handle()
+@del_sub.handle()
+async def _(session: EventSession, state: T_State, sub_type: str, sub_msg: str):
+    gid = session.id3 or session.id2
+    if gid:
+        sub_user = f"{session.id1}:{gid}"
+    else:
+        sub_user = f"{session.id1}"
+    state["sub_type"] = sub_type
+    state["sub_user"] = sub_user
+    if "http" in sub_msg:
+        sub_msg = sub_msg.split("?")[0]
+        sub_msg = sub_msg[:-1] if sub_msg[-1] == "/" else sub_msg
+        sub_msg = sub_msg.split("/")[-1]
+    id_ = sub_msg[2:] if sub_msg.startswith("md") else sub_msg
+    if not id_.isdigit():
+        if sub_type in ["season", "动漫", "番剧"]:
+            rst = "*以为您找到以下番剧，请输入Id选择：*\n"
+            state["season_data"] = await get_media_id(id_)
+            if len(state["season_data"]) == 0:
+                await MessageUtils.build_message(f"未找到番剧：{sub_msg}").finish()
+            for i, x in enumerate(state["season_data"]):
+                rst += f'{i + 1}.{state["season_data"][x]["title"]}\n----------\n'
+            await MessageUtils.build_message("\n".join(rst.split("\n")[:-1])).send()
+        else:
+            await MessageUtils.build_message("Id 必须为全数字！").finish()
+    else:
+        state["id"] = int(id_)
+
+
+@add_sub.got("sub_type")
+@add_sub.got("sub_user")
+@add_sub.got("id")
+async def _(
+        session: EventSession,
+        state: T_State,
+        id_: str = ArgStr("id"),
+        sub_type: str = ArgStr("sub_type"),
+        sub_user: str = ArgStr("sub_user"),
+):
+    if sub_type in ["season", "动漫", "番剧"] and state.get("season_data"):
+        season_data = state["season_data"]
+        if not id_.isdigit() or int(id_) < 1 or int(id_) > len(season_data):
+            await add_sub.reject_arg("id", "Id必须为数字且在范围内！请重新输入...")
+        id_ = season_data[int(id_) - 1]["media_id"]
+    id_ = int(id_)
+    if sub_type in ["主播", "直播"]:
+        await MessageUtils.build_message(await add_live_sub(id_, sub_user)).send()
+    elif sub_type.lower() in ["up", "用户"]:
+        await MessageUtils.build_message(await add_up_sub(id_, sub_user)).send()
+    elif sub_type in ["season", "动漫", "番剧"]:
+        await MessageUtils.build_message(await add_season_sub(id_, sub_user)).send()
+    else:
+        await MessageUtils.build_message("参数错误，第一参数必须为：主播/up/番剧！").finish()
+    gid = session.id3 or session.id2
+    logger.info(
+        f"(USER {session.id1}, GROUP "
+        f"{gid if gid else 'private'})"
+        f" 添加订阅：{sub_type} -> {sub_user} -> {id_}"
+    )
+
+
+@del_sub.got("sub_type")
+@del_sub.got("sub_user")
+@del_sub.got("id")
+async def _(
+        session: EventSession,
+        id_: str = ArgStr("id"),
+        sub_type: str = ArgStr("sub_type"),
+        sub_user: str = ArgStr("sub_user"),
+):
+    if sub_type in ["主播", "直播"]:
+        result = await BilibiliSub.delete_bilibili_sub(int(id_), sub_user, "live")
+    elif sub_type.lower() in ["up", "用户"]:
+        result = await BilibiliSub.delete_bilibili_sub(int(id_), sub_user, "up")
+    else:
+        result = await BilibiliSub.delete_bilibili_sub(int(id_), sub_user)
+    if result:
+        await MessageUtils.build_message(f"删除订阅id：{id_} 成功...").send()
+        gid = session.id3 or session.id2
+        logger.info(
+            f"(USER {session.id1}, GROUP "
+            f"{gid if gid else 'private'})"
+            f" 删除订阅 {id_}"
+        )
+    else:
+        await MessageUtils.build_message(f"删除订阅id：{id_} 失败...").send()
+
+
+@show_sub_info.handle()
+async def _(session: EventSession):
+    gid = session.id3 or session.id2
+    id_ = gid if gid else session.id1
+    data = await BilibiliSub.filter(sub_users__contains=id_).all()
+    live_rst = ""
+    up_rst = ""
+    season_rst = ""
+    for x in data:
+        if x.sub_type == "live":
+            live_rst += (
+                f"\t直播间id：{x.sub_id}\n" f"\t名称：{x.uname}\n" f"------------------\n"
+            )
+        if x.sub_type == "up":
+            up_rst += f"\tUP：{x.uname}\n" f"\tuid：{x.uid}\n" f"------------------\n"
+        if x.sub_type == "season":
+            season_rst += (
+                f"\t番剧id：{x.sub_id}\n"
+                f"\t番名：{x.season_name}\n"
+                f"\t当前集数：{x.season_current_episode}\n"
+                f"------------------\n"
+            )
+    live_rst = "当前订阅的直播：\n" + live_rst if live_rst else live_rst
+    up_rst = "当前订阅的UP：\n" + up_rst if up_rst else up_rst
+    season_rst = "当前订阅的番剧：\n" + season_rst if season_rst else season_rst
+    if not live_rst and not up_rst and not season_rst:
+        live_rst = (
+            "该群目前没有任何订阅..." if gid else "您目前没有任何订阅..."
+        )
+
+    img = await text2image(
+        live_rst + up_rst + season_rst, padding=10, color="#f9f6f2"
+    )
+    await MessageUtils.build_message(img).finish()
+
+
+@blive_check.handle()
+async def _():
+    if not AuthManager.grpc_auths:
+        await MessageUtils.build_message("没有缓存的登录信息").finish()
+    msgs = []
+    for auth in AuthManager.grpc_auths:
+        token_time = calc_time_total(auth.tokens_expired - int(time.time()))
+        cookie_time = calc_time_total(auth.cookies_expired - int(time.time()))
+        msg = (
+            f"账号uid: {auth.uid}\n"
+            f"token有效期: {token_time}\n"
+            f"cookie有效期: {cookie_time}"
+        )
+        msgs.append(msg)
+    await MessageUtils.build_message("\n----------\n".join(msgs)).finish()
+
+
+@blive_login.handle()
+async def _(matcher: Matcher):
+    login = Login()
+    qr_url = await login.get_qrcode_url()
+    logger.debug(f"qrcode login url: {qr_url}")
+    img = await login.get_qrcode(qr_url)
+    if not img:
+        await MessageUtils.build_message("获取二维码失败").finish()
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    img_data = buffered.getvalue()
+    await MessageUtils.build_message(img_data).send()
+    try:
+        auth = await login.qrcode_login(interval=5)
+        assert auth, "登录失败，返回数据为空"
+        logger.debug(auth.data)
+        AuthManager.add_auth(auth)
+    except Exception as e:
+        await MessageUtils.build_message(f"登录失败: {e}").finish()
+    await MessageUtils.build_message("登录成功，已将验证信息缓存至文件").finish()
+
+
+@blive_logout.handle()
+async def _(uid: int):
+    if msg := AuthManager.remove_auth(uid):
+        await MessageUtils.build_message(msg).finish()
+    await MessageUtils.build_message(f"账号 {uid} 已退出登录").finish()
+
+
+# 推送
+@scheduler.scheduled_job(
+    "interval",
+    seconds=base_config.get("CHECK_TIME") if base_config.get("CHECK_TIME") else 60,
+)
+async def _():
+    bots = nonebot.get_bots()
+    for bot in bots.values():
+        if bot:
+            # try:
+            await sub_manager.reload_sub_data()
+            sub = await sub_manager.random_sub_data()
+            if sub:
+                logger.info(f"Bilibili订阅开始检测：{sub.sub_id}")
+                msg_list = await get_sub_status(sub.sub_id, sub.sub_type)
+                if msg_list:
+                    await send_sub_msg(msg_list, sub, bot)
+                    if sub.sub_type == "live":
+                        msg_list = await get_sub_status(sub.sub_id, "up")
+                        await send_sub_msg(msg_list, sub, bot)
+            # except Exception as e:
+            #     logger.error(f"B站订阅推送发生错误 sub_id：{sub.sub_id if sub else 0} {type(e)}：{e}")
+
+
+async def send_sub_msg(msg_list: list, sub: BilibiliSub, bot: Bot):
+    """
+    推送信息
+    :param msg_list: 消息列表
+    :param sub: BilibiliSub
+    :param bot: Bot
+    """
+    temp_group = []
+    if msg_list:
+        for x in sub.sub_users.split(",")[:-1]:
+            try:
+                if ":" in x and x.split(":")[1] not in temp_group:
+                    group_id = x.split(":")[1]
+                    temp_group.append(group_id)
+                    if (
+                            await bot.get_group_member_info(
+                                group_id=int(group_id), user_id=int(bot.self_id), no_cache=True
+                            )
+                    )["role"] in ["owner", "admin"]:
+                        if (
+                                sub.sub_type == "live"
+                                and Config.get_config("bilibili_sub", "LIVE_MSG_AT_ALL")
+                        ) or (
+                                sub.sub_type == "up"
+                                and Config.get_config("bilibili_sub", "UP_MSG_AT_ALL")
+                        ):
+                            msg_list.append(UniMessage.at_all())
+                    if not await GroupConsole.is_block_plugin(group_id, "bilibili_sub"):
+                        await PlatformUtils.send_message(bot, user_id=None, group_id=group_id,
+                                                         message=MessageUtils.build_message(msg_list))
+
+                else:
+                    await PlatformUtils.send_message(bot, user_id=x, group_id=None,
+                                                     message=MessageUtils.build_message(msg_list))
+            except Exception as e:
+                logger.error(f"B站订阅推送发生错误 sub_id：{sub.sub_id} {type(e)}：{e}")
---
+++ plugins/bilibili_sub/data_source.py
@@ -0,0 +1,430 @@
+import random
+from asyncio.exceptions import TimeoutError
+from datetime import datetime
+from typing import Optional, Tuple
+
+import nonebot
+from bilireq.exceptions import ResponseCodeError  # type: ignore
+
+from zhenxun.configs.path_config import IMAGE_PATH
+from zhenxun.services.log import logger
+from zhenxun.utils.http_utils import AsyncHttpx
+from zhenxun.utils.utils import ResourceDirManager
+from zhenxun.utils._build_image import BuildImage
+from zhenxun.utils.platform import PlatformUtils
+
+from .model import BilibiliSub
+from .utils import (
+    get_meta,
+    get_user_card,
+    get_room_info_by_id,
+    get_videos,
+    get_user_dynamics,
+    get_dynamic_screenshot,
+)
+
+
+SEARCH_URL = "https://api.bilibili.com/x/web-interface/search/all/v2"
+
+DYNAMIC_PATH = IMAGE_PATH / "bilibili_sub" / "dynamic"
+DYNAMIC_PATH.mkdir(exist_ok=True, parents=True)
+
+ResourceDirManager.add_temp_dir(DYNAMIC_PATH)
+
+
+async def handle_video_info_error(video_info: dict):
+    """
+
+    处理B站视频信息获取错误并发送通知给超级用户
+    :param video_info: 包含错误信息的字典
+    :param platform_utils: 用于发送消息的工具类
+    """
+    str_msg = "b站订阅检测失败："
+    if video_info["code"] == -352:
+        str_msg += "风控校验失败，请登录后再尝试。发送'登录b站'"
+    elif video_info["code"] == -799:
+        str_msg += "请求过于频繁，请增加时长，更改配置文件下的'CHECK_TIME''"
+    else:
+        str_msg += f"{video_info['code']}，{video_info['message']}"
+
+    bots = nonebot.get_bots()
+    for bot in bots.values():
+        if bot:
+            await PlatformUtils.send_superuser(bot, str_msg)
+
+    return str_msg
+
+
+async def add_live_sub(live_id: int, sub_user: str) -> str:
+    """
+
+    添加直播订阅
+    :param live_id: 直播房间号
+    :param sub_user: 订阅用户 id # 7384933:private or 7384933:2342344(group)
+    :return:
+    """
+    try:
+        try:
+            """bilibili_api.live库的LiveRoom类中get_room_info改为bilireq.live库的get_room_info_by_id方法"""
+            live_info = await get_room_info_by_id(live_id)
+        except ResponseCodeError:
+            return f"未找到房间号Id：{live_id} 的信息，请检查Id是否正确"
+        uid = live_info["uid"]
+        room_id = live_info["room_id"]
+        short_id = live_info["short_id"]
+        title = live_info["title"]
+        live_status = live_info["live_status"]
+        if await BilibiliSub.sub_handle(
+            room_id,
+            "live",
+            sub_user,
+            uid=uid,
+            live_short_id=short_id,
+            live_status=live_status,
+        ):
+            await _get_up_status(room_id)
+            uname = (await BilibiliSub.get_or_none(sub_id=room_id)).uname
+            return (
+                "订阅成功！🎉\n"
+                f"主播名称：{uname}\n"
+                f"直播标题：{title}\n"
+                f"直播间ID：{room_id}\n"
+                f"用户UID：{uid}"
+            )
+        else:
+            return "添加订阅失败..."
+    except Exception as e:
+        logger.error(f"订阅主播live_id：{live_id} 发生了错误 {type(e)}：{e}")
+    return "添加订阅失败..."
+
+
+async def add_up_sub(uid: int, sub_user: str) -> str:
+    """
+    添加订阅 UP
+    :param uid: UP uid
+    :param sub_user: 订阅用户
+    """
+    try:
+        try:
+            """bilibili_api.user库中User类的get_user_info改为bilireq.user库的get_user_info方法"""
+            user_info = await get_user_card(uid)
+        except ResponseCodeError:
+            return f"未找到UpId：{uid} 的信息，请检查Id是否正确"
+        uname = user_info["name"]
+        try:
+            dynamic_info = await get_user_dynamics(uid)
+        except ResponseCodeError as e:
+            if e.code == -352:
+                return "风控校验失败，请联系管理员登录b站'"
+            return "添加订阅失败..."
+        dynamic_upload_time = 0
+        if dynamic_info.get("cards"):
+            dynamic_upload_time = dynamic_info["cards"][0]["desc"]["timestamp"]
+        """bilibili_api.user库中User类的get_videos改为bilireq.user库的get_videos方法"""
+        video_info = await get_videos(uid)
+        if not video_info.get("data"):
+            await handle_video_info_error(video_info)
+            return "订阅失败，请联系管理员"
+        else:
+            video_info = video_info["data"]
+        latest_video_created = 0
+        if video_info["list"].get("vlist"):
+            latest_video_created = video_info["list"]["vlist"][0]["created"]
+        if await BilibiliSub.sub_handle(
+            uid,
+            "up",
+            sub_user,
+            uid=uid,
+            uname=uname,
+            dynamic_upload_time=dynamic_upload_time,
+            latest_video_created=latest_video_created,
+        ):
+            return "订阅成功！🎉\n" f"UP主名称：{uname}\n" f"用户UID：{uid}"
+        else:
+            return "添加订阅失败..."
+    except Exception as e:
+        logger.error(f"订阅Up uid：{uid} 发生了错误 {type(e)}：{e}")
+    return "添加订阅失败..."
+
+
+async def add_season_sub(media_id: int, sub_user: str) -> str:
+    """
+    添加订阅 UP
+    :param media_id: 番剧 media_id
+    :param sub_user: 订阅用户
+    """
+    try:
+        try:
+            """bilibili_api.bangumi库中get_meta改为bilireq.bangumi库的get_meta方法"""
+            season_info = await get_meta(media_id)
+        except ResponseCodeError:
+            return f"未找到media_id：{media_id} 的信息，请检查Id是否正确"
+        season_id = season_info["media"]["season_id"]
+        season_current_episode = season_info["media"]["new_ep"]["index"]
+        season_name = season_info["media"]["title"]
+        if await BilibiliSub.sub_handle(
+            media_id,
+            "season",
+            sub_user,
+            season_name=season_name,
+            season_id=season_id,
+            season_current_episode=season_current_episode,
+        ):
+            return (
+                "订阅成功！🎉\n"
+                f"番剧名称：{season_name}\n"
+                f"当前集数：{season_current_episode}"
+            )
+        else:
+            return "添加订阅失败..."
+    except Exception as e:
+        logger.error(f"订阅番剧 media_id：{media_id} 发生了错误 {type(e)}：{e}")
+    return "添加订阅失败..."
+
+
+async def delete_sub(sub_id: str, sub_user: str) -> str:
+    """
+    删除订阅
+    :param sub_id: 订阅 id
+    :param sub_user: 订阅用户 id # 7384933:private or 7384933:2342344(group)
+    """
+    if await BilibiliSub.delete_bilibili_sub(int(sub_id), sub_user):
+        return f"已成功取消订阅：{sub_id}"
+    else:
+        return f"取消订阅：{sub_id} 失败，请检查是否订阅过该Id...."
+
+
+async def get_media_id(keyword: str) -> dict:
+    """
+    获取番剧的 media_id
+    :param keyword: 番剧名称
+    """
+    from .auth import AuthManager
+
+    params = {"keyword": keyword}
+    for _ in range(3):
+        try:
+            _season_data = {}
+            response = await AsyncHttpx.get(
+                SEARCH_URL, params=params, cookies=AuthManager.get_cookies(), timeout=5
+            )
+            if response.status_code == 200:
+                data = response.json()
+                if data.get("data"):
+                    for item in data["data"]["result"]:
+                        if item["result_type"] == "media_bangumi":
+                            idx = 0
+                            for x in item["data"]:
+                                _season_data[idx] = {
+                                    "media_id": x["media_id"],
+                                    "title": x["title"]
+                                    .replace('<em class="keyword">', "")
+                                    .replace("</em>", ""),
+                                }
+                                idx += 1
+                            return _season_data
+        except TimeoutError:
+            pass
+        return {}
+
+
+async def get_sub_status(id_: int, sub_type: str) -> list | None:
+    """
+    获取订阅状态
+    :param id_: 订阅 id
+    :param sub_type: 订阅类型
+    """
+    try:
+        if sub_type == "live":
+            return await _get_live_status(id_)
+        elif sub_type == "up":
+            return await _get_up_status(id_)
+        elif sub_type == "season":
+            return await _get_season_status(id_)
+    except ResponseCodeError as msg:
+        logger.info(f"Id：{id_} 获取信息失败...{msg}")
+        return None
+        # return f"Id：{id_} 获取信息失败...请检查订阅Id是否存在或稍后再试..."
+    # except Exception as e:
+    #     logger.error(f"获取订阅状态发生预料之外的错误 id_：{id_} {type(e)}：{e}")
+    #     return "发生了预料之外的错误..请稍后再试或联系管理员....."
+
+
+async def _get_live_status(id_: int) -> list:
+    """
+    获取直播订阅状态
+    :param id_: 直播间 id
+    """
+    """bilibili_api.live库的LiveRoom类中get_room_info改为bilireq.live库的get_room_info_by_id方法"""
+    live_info = await get_room_info_by_id(id_)
+    title = live_info["title"]
+    room_id = live_info["room_id"]
+    live_status = live_info["live_status"]
+    cover = live_info["user_cover"]
+    sub = await BilibiliSub.get_or_none(sub_id=id_)
+    msg_list = []
+    if sub.live_status != live_status:
+        await BilibiliSub.sub_handle(id_, live_status=live_status)
+    if sub.live_status in [0, 2] and live_status == 1:
+        msg_list = [
+            BuildImage(cover),
+            "\n",
+            f"{sub.uname} 开播啦！🎉\n",
+            f"标题：{title}\n",
+            f"直播间链接：https://live.bilibili.com/{room_id}",
+        ]
+    return msg_list
+
+
+async def _get_up_status(id_: int) -> list:
+    """
+    获取用户投稿状态
+    :param id_: 订阅 id
+    :return:
+    """
+    _user = await BilibiliSub.get_or_none(sub_id=id_)
+    """bilibili_api.user库中User类的get_user_info改为bilireq.user库的get_user_info方法"""
+    user_info = await get_user_card(_user.uid)
+    uname = user_info["name"]
+    """bilibili_api.user库中User类的get_videos改为bilireq.user库的get_videos方法"""
+    video_info = await get_videos(_user.uid)
+    if not video_info.get("data"):
+        await handle_video_info_error(video_info)
+        return []
+    else:
+        video_info = video_info["data"]
+    latest_video_created = 0
+    video = None
+    dividing_line = "\n-------------\n"
+    if _user.uname != uname:
+        await BilibiliSub.sub_handle(id_, uname=uname)
+    try:
+        dynamic_img, dynamic_upload_time, link = await get_user_dynamic(_user.uid, _user)
+    except ResponseCodeError as msg:
+        logger.warning(f"Id：{id_} 获取信息失败...{msg}")
+    if video_info["list"].get("vlist"):
+        video = video_info["list"]["vlist"][0]
+        latest_video_created = video["created"]
+    msg_list = []
+    if dynamic_img:
+        await BilibiliSub.sub_handle(id_, dynamic_upload_time=dynamic_upload_time)
+        msg_list = [f"{uname} 发布了动态！📢\n", dynamic_img, f"\n查看详情：{link}"]
+    if (
+        latest_video_created
+        and _user.latest_video_created
+        and video
+        and _user.latest_video_created < latest_video_created
+    ):
+        await BilibiliSub.sub_handle(id_, latest_video_created=latest_video_created)
+
+        if msg_list:
+            msg_list.append(dividing_line)
+        msg_list.append(BuildImage(video["pic"]))
+        msg_list.append(
+            "\n"
+            f"{uname} 投稿了新视频啦！🎉\n"
+            f"标题：{video['title']}\n"
+            f"Bvid：{video['bvid']}\n"
+            f"视频链接：https://www.bilibili.com/video/{video['bvid']}"
+        )
+    return msg_list
+
+
+async def _get_season_status(id_) -> list:
+    """
+    获取 番剧 更新状态
+    :param id_: 番剧 id
+    """
+    """bilibili_api.bangumi库中get_meta改为bilireq.bangumi库的get_meta方法"""
+    season_info = await get_meta(id_)
+    title = season_info["media"]["title"]
+    _idx = (await BilibiliSub.get_or_none(sub_id=id_)).season_current_episode
+    new_ep = season_info["media"]["new_ep"]["index"]
+    msg_list = []
+    if new_ep != _idx:
+        await BilibiliSub.sub_handle(
+            id_, season_current_episode=new_ep, season_update_time=datetime.now()
+        )
+        msg_list = [
+            BuildImage(season_info["media"]["cover"]),
+            "\n",
+            f"[{title}] 更新啦！🎉\n",
+            f"最新集数：{new_ep}",
+        ]
+    return msg_list
+
+
+async def get_user_dynamic(
+    uid: int, local_user: BilibiliSub
+) -> Tuple[bytes | None, int, str]:
+    """
+    获取用户动态
+    :param uid: 用户uid
+    :param local_user: 数据库存储的用户数据
+    :return: 最新动态截图与时间
+    """
+    dynamic_info = await get_user_dynamics(uid)
+    if dynamic_info.get("cards"):
+        dynamic_upload_time = dynamic_info["cards"][0]["desc"]["timestamp"]
+        dynamic_id = dynamic_info["cards"][0]["desc"]["dynamic_id"]
+        if local_user.dynamic_upload_time < dynamic_upload_time:
+            image = await get_dynamic_screenshot(dynamic_id)
+            return (
+                image,
+                dynamic_upload_time,
+                f"https://t.bilibili.com/{dynamic_id}",
+            )
+    return None, 0, ""
+
+
+class SubManager:
+    def __init__(self):
+        self.live_data = []
+        self.up_data = []
+        self.season_data = []
+        self.current_index = -1
+
+    async def reload_sub_data(self):
+        """
+        重载数据
+        """
+        if not self.live_data or not self.up_data or not self.season_data:
+            (
+                _live_data,
+                _up_data,
+                _season_data,
+            ) = await BilibiliSub.get_all_sub_data()
+            if not self.live_data:
+                self.live_data = _live_data
+            if not self.up_data:
+                self.up_data = _up_data
+            if not self.season_data:
+                self.season_data = _season_data
+
+    async def random_sub_data(self) -> Optional[BilibiliSub]:
+        """
+        随机获取一条数据
+        :return:
+        """
+        sub = None
+        if not self.live_data and not self.up_data and not self.season_data:
+            return sub
+        self.current_index += 1
+        if self.current_index == 0:
+            if self.live_data:
+                sub = random.choice(self.live_data)
+                self.live_data.remove(sub)
+        elif self.current_index == 1:
+            if self.up_data:
+                sub = random.choice(self.up_data)
+                self.up_data.remove(sub)
+        elif self.current_index == 2:
+            if self.season_data:
+                sub = random.choice(self.season_data)
+                self.season_data.remove(sub)
+        else:
+            self.current_index = -1
+        if sub:
+            return sub
+        await self.reload_sub_data()
+        return await self.random_sub_data()
