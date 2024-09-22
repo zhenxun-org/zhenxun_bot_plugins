4cabc8b67335457e10a1bc085d0f241a1af89608
AkashiCoin <l1040186796@gmail.com>
2024-09-22 16:27:26 +0000

    chore: update plugin version

--- plugins/word_bank/__init__.py
+++ plugins/word_bank/__init__.py
@@ -12,7 +12,7 @@ __plugin_meta__ = PluginMetadata(
     usage="",
     extra=PluginExtraData(
         author="HibiKier",
-        version="0.5",
+        version="0.5-6fcedc5",
         plugin_type=PluginType.PARENT,
         configs=[
             RegisterConfig(
