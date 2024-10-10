9f4a871fffcb9595b244771dd0b4c0d2b030561f
ATTomato <93612024+ATTomatoo@users.noreply.github.com>
2024-10-10 16:11:42 +0800

    Update __init__.py
    update plugin version

--- plugins/mute/__init__.py
+++ plugins/mute/__init__.py
@@ -12,7 +12,7 @@ __plugin_meta__ = PluginMetadata(
     usage="",
     extra=PluginExtraData(
         author="HibiKier",
-        version="0.1-ea238a0",
+        version="0.1-473ecd8",
         plugin_type=PluginType.PARENT,
     ).dict(),
 )
