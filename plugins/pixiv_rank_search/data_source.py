5b3eba443130df751250533fba0a49662896292e
HibiKier <775757368@qq.com>
2024-11-08 22:31:06 +0800

    🐛 修复p站排行/搜图

--- plugins/pixiv_rank_search/data_source.py
+++ plugins/pixiv_rank_search/data_source.py
@@ -1,11 +1,14 @@
 from asyncio.exceptions import TimeoutError
 from pathlib import Path
+import random
 
 from zhenxun.configs.config import Config
 from zhenxun.configs.path_config import TEMP_PATH
 from zhenxun.services.log import logger
 from zhenxun.utils.http_utils import AsyncHttpx
 from zhenxun.utils.utils import change_img_md5
+
+base_config = Config.get("pixiv_rank_search")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6;"
@@ -26,7 +29,7 @@ async def get_pixiv_urls(
         date: 日期.
 
     返回:
-        tuple[list[tuple[str, str, list[str]] | str], int]: 图片标题作者url数据，请求状态
+        tuple[list[tuple[str, str, list[str]] | str], int]:图片标题作者url数据，请求状态
     """
 
     params = {"mode": mode, "page": page}
@@ -50,7 +53,7 @@ async def search_pixiv_urls(
         r18: 是否r18
 
     返回:
-        tuple[list[tuple[str, str, list[str]] | str], int]: 图片标题作者url数据，请求状态
+        tuple[list[tuple[str, str, list[str]] | str], int]:图片标题作者url数据，请求状态
     """
     params = {"word": keyword, "page": page}
     hibiapi = Config.get_config("hibiapi", "HIBIAPI")
@@ -72,7 +75,7 @@ async def parser_data(
         r18: 是否r18.
 
     返回:
-        tuple[list[tuple[str, str, list[str]] | str], int]: 图片标题作者url数据，请求状态
+        tuple[list[tuple[str, str, list[str]] | str], int]:图片标题作者url数据，请求状态
     """
     info_list = []
     for _ in range(3):
@@ -80,7 +83,7 @@ async def parser_data(
             response = await AsyncHttpx.get(
                 url,
                 params=params,
-                timeout=Config.get_config("pixiv_rank_search", "TIMEOUT"),
+                timeout=base_config.get("TIMEOUT"),
             )
             if response.status_code == 200:
                 data = response.json()
@@ -90,20 +93,20 @@ async def parser_data(
         except TimeoutError:
             pass
         except Exception as e:
-            logger.error(f"P站排行/搜图解析数据发生错误", e=e)
+            logger.error("P站排行/搜图解析数据发生错误", e=e)
             return ["发生了一些些错误..."], 995
     else:
         return ["网络不太好？没有该页数？也许过一会就好了..."], 998
-    num = num if num < 30 else 30
+    num = min(num, 30)
     _data = []
     for x in data:
-        if x["page_count"] < Config.get_config("pixiv_rank_search", "MAX_PAGE_LIMIT"):
-            if type_ == "search" and r18 == 1:
-                if "R-18" in str(x["tags"]):
-                    continue
+        if x["page_count"] < base_config.get("MAX_PAGE_LIMIT"):
+            if type_ == "search" and r18 == 1 and "R-18" in str(x["tags"]):
+                continue
             _data.append(x)
         if len(_data) == num:
             break
+    pixiv_nginx = Config.get_config("pixiv", "PIXIV_NGINX_URL")
     for x in _data:
         title = x["title"]
         author = x["user"]["name"]
@@ -113,7 +116,16 @@ async def parser_data(
         else:
             for j in x["meta_pages"]:
                 urls.append(j["image_urls"]["large"])
-        info_list.append((title, author, urls))
+        img_type = urls[0].rsplit(".")[-1]
+        url_list = []
+        if len(urls) == 1:
+            img_url = f"https://{pixiv_nginx}/{x['id']}.{img_type}"
+            url_list.append(img_url)
+        else:
+            for i, _ in enumerate(urls):
+                img_url = f"https://{pixiv_nginx}/{x['id']}-{i+1}.{img_type}"
+                url_list.append(img_url)
+        info_list.append((title, author, url_list))
     return info_list, 200
 
 
@@ -131,24 +143,20 @@ async def download_pixiv_imgs(
         MessageFactory: 图片
     """
     result_list = []
-    index = 0
     for url in urls:
-        ws_url = Config.get_config("pixiv", "PIXIV_NGINX_URL")
-        url = url.replace("_webp", "")
-        if ws_url:
-            url = url.replace("i.pximg.net", ws_url).replace("i.pixiv.cat", ws_url)
+        index = random.randint(1, 10000)
+        img_type = url.rsplit(".")[-1]
         try:
             file = (
-                TEMP_PATH / f"{user_id}_{forward_msg_index}_{index}_pixiv.jpg"
+                TEMP_PATH / f"{user_id}_{forward_msg_index}_{index}_pixiv.{img_type}"
                 if forward_msg_index is not None
-                else TEMP_PATH / f"{user_id}_{index}_pixiv.jpg"
+                else TEMP_PATH / f"{user_id}_{index}_pixiv.{img_type}"
             )
-            file = Path(file)
             try:
                 if await AsyncHttpx.download_file(
                     url,
                     file,
-                    timeout=Config.get_config("pixiv_rank_search", "TIMEOUT"),
+                    timeout=base_config.get("TIMEOUT"),
                     headers=headers,
                 ):
                     change_img_md5(file)
@@ -156,16 +164,15 @@ async def download_pixiv_imgs(
                     if forward_msg_index is not None:
                         image = (
                             TEMP_PATH
-                            / f"{user_id}_{forward_msg_index}_{index}_pixiv.jpg"
+                            / f"{user_id}_{forward_msg_index}_{index}_pixiv.{img_type}"
                         )
                     else:
-                        image = TEMP_PATH / f"{user_id}_{index}_pixiv.jpg"
+                        image = TEMP_PATH / f"{user_id}_{index}_pixiv.{img_type}"
                     if image:
                         result_list.append(image)
-                index += 1
             except OSError:
                 if file.exists():
                     file.unlink()
         except Exception as e:
-            logger.error(f"P站排行/搜图下载图片错误", e=e)
+            logger.error("P站排行/搜图下载图片错误", e=e)
     return result_list
