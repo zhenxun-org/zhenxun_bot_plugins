8b45b37e5ce3bb8ce1aebf45148b2960140fe54b
HibiKier <775757368@qq.com>
2024-11-10 16:04:58 +0800

    🐛 修复识番提交图片错误

--- plugins/what_anime/data_source.py
+++ plugins/what_anime/data_source.py
@@ -1,47 +1,29 @@
-import time
-
 from zhenxun.services.log import logger
 from zhenxun.utils.http_utils import AsyncHttpx
+from nonebot_plugin_alconna import Image
 
 
-async def get_anime(anime: str) -> str:
-    s_time = time.time()
-    url = "https://api.trace.moe/search?anilistInfo&url={}".format(anime)
-    logger.debug("[info]Now starting get the {}".format(url))
+async def get_anime(anime: str) -> str | list:
+    anime = anime.replace("&", "%26")
+    url = f"https://api.trace.moe/search?anilistInfo&url={anime}"
+    logger.debug(f"Now starting get the {url}")
     try:
-        anime_json = (await AsyncHttpx.get(url)).json()
-        if not anime_json["error"]:
-            if anime_json == "Error reading imagenull":
-                return "图像源错误，注意必须是静态图片哦"
-            repass = ""
-            # 拿到动漫 中文名
-            for anime in anime_json["result"][:5]:
-                synonyms = anime["anilist"]["synonyms"]
-                for x in synonyms:
-                    _count_ch = 0
-                    for word in x:
-                        if "\u4e00" <= word <= "\u9fff":
-                            _count_ch += 1
-                    if _count_ch > 3:
-                        anime_name = x
-                        break
-                else:
-                    anime_name = anime["anilist"]["title"]["native"]
-                episode = anime["episode"]
-                from_ = int(anime["from"])
-                m, s = divmod(from_, 60)
-                similarity = anime["similarity"]
-                putline = "[ {} ][{}][{}:{}] 相似度:{:.2%}".format(
-                    anime_name,
-                    episode if episode else "?",
-                    m,
-                    s,
-                    similarity,
-                )
-                repass += putline + "\n"
-            return f"耗时 {int(time.time() - s_time)} 秒\n" + repass[:-1]
-        else:
+        anime_json: dict = (await AsyncHttpx.get(url)).json()
+        if anime_json == "Error reading imagenull":
+            return "图像源错误，注意必须是静态图片哦"
+        if anime_json["error"]:
             return f'访问错误 error：{anime_json["error"]}'
+        return [
+            [
+                f"名称: {anime['filename'].rsplit('.')[0]}\n"
+                f"集数: {anime['episode']}\n"
+                f"相似度: {anime['similarity']:.2f}%\n"
+                f"图片:",
+                Image(url=anime["image"]),
+                "----------\n",
+            ]
+            for anime in anime_json["result"][:5]
+        ]
     except Exception as e:
-        logger.error(f"识番发生错误", e=e)
+        logger.error("识番发生错误", e=e)
         return "发生了奇怪的错误，那就没办法了，再试一次？"
