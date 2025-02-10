ce13139f00469470d0b797698a3d643c00e9f45d
chtholly <2010768120@qq.com>
2025-02-11 00:26:13 +0800

    新增伪人AI插件

---
+++ plugins/bym_ai/__init__.py
@@ -0,0 +1,120 @@
+import asyncio
+import random
+
+from nonebot import on_message
+from nonebot.adapters import Event
+from nonebot.plugin import PluginMetadata
+from nonebot_plugin_alconna import UniMsg, Voice
+from nonebot_plugin_uninfo import Uninfo
+
+from zhenxun.configs.config import BotConfig
+from zhenxun.configs.utils import PluginExtraData, RegisterConfig
+from zhenxun.services.log import logger
+from zhenxun.utils.depends import UserName
+from zhenxun.utils.message import MessageUtils
+
+from .data_source import ChatManager, base_config, split_text
+
+__plugin_meta__ = PluginMetadata(
+    name="BYM_AI",
+    description=f"{BotConfig.self_nickname}想成为人类...",
+    usage=f"""
+    你问小真寻的愿望？
+    {BotConfig.self_nickname}说她想成为人类！
+    """.strip(),
+    extra=PluginExtraData(
+        author="Chtholly",
+        version="0.1",
+        configs=[
+            RegisterConfig(
+                key="BYM_AI_CHAT_URL",
+                value=None,
+                help="ai聊天接口地址",
+            ),
+            RegisterConfig(
+                key="BYM_AI_CHAT_TOKEN",
+                value=None,
+                help="ai聊天接口密钥",
+            ),
+            RegisterConfig(
+                key="BYM_AI_CHAT_MODEL",
+                value=None,
+                help="ai聊天接口模型",
+            ),
+            RegisterConfig(
+                key="BYM_AI_CHAT",
+                value=True,
+                help="是否开启伪人回复",
+                default_value=True,
+                type=bool,
+            ),
+            RegisterConfig(
+                key="BYM_AI_CHAT_RATE",
+                value=5,
+                help="伪人回复概率 0-100",
+                default_value=5,
+                type=int,
+            ),
+            RegisterConfig(
+                key="BYM_AI_TTS_URL",
+                value=None,
+                help="tts接口地址",
+            ),
+            RegisterConfig(
+                key="BYM_AI_TTS_TOKEN",
+                value=None,
+                help="tts接口密钥",
+            ),
+            RegisterConfig(
+                key="BYM_AI_TTS_VOICE",
+                value=None,
+                help="tts接口音色",
+            ),
+        ],
+    ).dict(),
+)
+
+
+async def rule(event: Event, session: Uninfo) -> bool:
+    if event.is_tome():
+        """at自身必定回复"""
+        return True
+    if not base_config.get("BYM_AI_CHAT"):
+        return False
+    if event.is_tome() and not session.group:
+        """私聊过滤"""
+        return False
+    rand = random.randint(1, 100)
+    rate = base_config.get("BYM_AI_CHAT_RATE")
+    if rand <= rate:
+        return True
+    return False
+
+
+_matcher = on_message(priority=998, rule=rule)
+
+
+@_matcher.handle()
+async def _(event: Event, message: UniMsg, session: Uninfo, uname: str = UserName()):
+    if not message.extract_plain_text().strip():
+        if event.is_tome():
+            await MessageUtils.build_message(ChatManager.hello()).finish()
+        return
+    group_id = session.group.id if session.group else ""
+    is_bym = not event.is_tome()
+    result = await ChatManager.get_result(
+        session.user.id, group_id, uname, message, is_bym
+    )
+    if is_bym:
+        """伪人回复，切割文本"""
+        if result:
+            for r, delay in split_text(result):
+                await MessageUtils.build_message(r).send()
+                await asyncio.sleep(delay)
+    else:
+        if not result:
+            return MessageUtils.build_message(ChatManager.no_result()).finish()
+        await MessageUtils.build_message(result).send()
+        if tts_data := await ChatManager.tts(result):
+            await MessageUtils.build_message(Voice(raw=tts_data)).send()
+    logger.info(f"BYM AI 问题: {message} | 回答: {result}", "BYM AI", session=session)
---
+++ plugins/bym_ai/config.py
@@ -0,0 +1,70 @@
+import os
+from typing import Any
+
+from pydantic import BaseModel
+
+from zhenxun.configs.config import BotConfig
+from zhenxun.configs.path_config import DATA_PATH, IMAGE_PATH
+
+PROMPT_FILE = DATA_PATH / "bym_ai" / "prompt.txt"
+PROMPT_FILE.parent.mkdir(parents=True, exist_ok=True)
+PROMPT_FILE.touch(exist_ok=True)
+
+BYM_CONTENT = """
+你在一个qq群里，群号是{group_id},
+当前和你说话的人昵称是{nickname},
+qq号是{user_id}, 请你结合用户的发言和聊天记录作出回应，
+要求表现得随性一点，最好参与讨论，混入其中。不要过分插科打诨，
+不知道说什么可以复读群友的话。要求优先使用中文进行对话。
+如果此时不需要自己说话，可以只回复<EMPTY>\n 下面是群组的聊天记录：
+"""
+
+NORMAL_CONTENT = """
+当前和你说话的人昵称是{nickname}，不要过多关注用户信息，请你着重结合用户的发言直接作出回应
+"""
+TIP_CONTENT = """
+你的回复应该尽可能简练，像人类一样随意，不要附加任何奇怪的东西，如聊天记录的格式，禁止重复聊天记录。
+"""
+
+
+NO_RESULT = [
+    "你在说啥子？",
+    f"纯洁的{BotConfig.self_nickname}没听懂",
+    "下次再告诉你(下次一定)",
+    "你觉得我听懂了吗？嗯？",
+    "我！不！知！道！",
+]
+
+NO_RESULT_IMAGE = os.listdir(IMAGE_PATH / "noresult")
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+    refusal: Any | None = None
+
+
+class Choices(BaseModel):
+    index: int
+    message: Message
+    logprobs: Any | None = None
+    finish_reason: str
+
+
+class Usage(BaseModel):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+    prompt_tokens_details: dict | None = None
+    completion_tokens_details: dict | None = None
+
+
+class OpenAiResult(BaseModel):
+    id: str | None = None
+    object: str
+    created: int
+    model: str
+    choices: list[Choices]
+    usage: Usage
+    service_tier: str | None = None
+    system_fingerprint: str | None = None
