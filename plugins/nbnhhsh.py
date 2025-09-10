08e704e18924b914772061b258b468ec73ec99d4
molanp <104612722+molanp@users.noreply.github.com>
2025-09-10 23:58:12 +0800

    优化排版

--- plugins/nbnhhsh.py
+++ plugins/nbnhhsh.py
@@ -47,7 +47,6 @@ async def _(session: EventSession, arparma: Arparma, text: str):
     )
     try:
         data = response.json()
-        tmp = ""
         result = ""
         for x in data:
             trans = ""
@@ -55,13 +54,12 @@ async def _(session: EventSession, arparma: Arparma, text: str):
                 trans = "，".join(x["trans"])
             elif x.get("inputting"):
                 trans = "，".join(x["inputting"])
-            tmp += f"{x['name']} -> {trans}\n"
-            result += trans
+            result += f"{x['name']} -> {trans}\n"
         logger.info(
             f" 发送能不能好好说话: {text} -> {result}",
             arparma.header_result,
             session=session,
         )
-        await MessageUtils.build_message(f"{tmp}={result}").send(reply_to=True)
+        await MessageUtils.build_message(result).send(reply_to=True)
     except (IndexError, KeyError):
         await MessageUtils.build_message("没有找到对应的翻译....").send()
