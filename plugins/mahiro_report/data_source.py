2995c5800a0367e3e3bfaf42ad3050f8eff287f1
LiZhao <weilizhao121@outlook.com>
2026-01-08 13:13:17 +0800

    真寻日报：添加API异常时的异常处理

--- plugins/mahiro_report/data_source.py
+++ plugins/mahiro_report/data_source.py
@@ -1,12 +1,13 @@
 import asyncio
+import xml.etree.ElementTree as ET
 from datetime import datetime
 from pathlib import Path
 
-import xml.etree.ElementTree as ET
 from zhdate import ZhDate
 
 from zhenxun import ui
 from zhenxun.configs.config import Config
+from zhenxun.services.log import logger
 from zhenxun.utils.http_utils import AsyncHttpx
 
 from .config import REPORT_PATH, Anime, Hitokoto, SixData
@@ -73,17 +74,25 @@ class Report:
 
     @classmethod
     async def get_hitokoto(cls) -> str:
-        """获取一言"""
-        res = await AsyncHttpx.get(cls.hitokoto_url)
-        data = Hitokoto(**res.json())
-        return data.hitokoto
+        """获取今日一言"""
+        try:
+            res = await AsyncHttpx.get(cls.hitokoto_url)
+            data = Hitokoto(**res.json())
+            return data.hitokoto
+        except Exception as e:
+            logger.error(f"获取今日一言失败: {e}")
+            return "获取今日一言失败 QAQ"
 
     @classmethod
     async def get_bili(cls) -> list[str]:
-        """获取哔哩哔哩热搜"""
-        res = await AsyncHttpx.get(cls.bili_url)
-        data = res.json()
-        return [item["keyword"] for item in data["list"]]
+        """获取B站热点"""
+        try:
+            res = await AsyncHttpx.get(cls.bili_url)
+            data = res.json()
+            return [item["keyword"] for item in data["list"]]
+        except Exception as e:
+            logger.error(f"获取B站热点失败: {e}")
+            return ["获取B站热点失败 QAQ"]
 
     @classmethod
     async def get_alapi_data(cls) -> list[str]:
@@ -91,45 +100,61 @@ class Report:
         token = Config.get_config("alapi", "ALAPI_TOKEN")  # 从配置中获取alapi
         payload = {"token": token, "format": "json"}
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
-        res = await AsyncHttpx.post(cls.alapi_url, data=payload, headers=headers)
-        if res.status_code != 200:
-            return ["Error: Unable to fetch data"]
-        data = res.json()
-        news_items = data.get("data", {}).get("news", [])
-        return news_items[:11] if len(news_items) > 11 else news_items
+        try:
+            res = await AsyncHttpx.post(cls.alapi_url, data=payload, headers=headers)
+            if res.status_code != 200:
+                return ["Error: Unable to fetch data"]
+            data = res.json()
+            news_items = data.get("data", {}).get("news", [])
+            return news_items[:11] if len(news_items) > 11 else news_items
+        except Exception as e:
+            logger.error(f"获取alapi数据失败: {e}")
+            return ["获取alapi数据失败 QAQ"]
 
     @classmethod
     async def get_six(cls) -> list[str]:
-        """获取60s数据"""
+        """获取60S读世界"""
         if Config.get_config("alapi", "ALAPI_TOKEN"):
             return await cls.get_alapi_data()
-        res = await AsyncHttpx.get(cls.six_url)
-        data = SixData(**res.json())
-        return data.data.news[:11] if len(data.data.news) > 11 else data.data.news
+        try:
+            res = await AsyncHttpx.get(cls.six_url)
+            data = SixData(**res.json())
+            return data.data.news[:11] if len(data.data.news) > 11 else data.data.news
+        except Exception as e:
+            logger.error(f"获取60S读世界失败: {e}")
+            return ["获取60S读世界失败 QAQ"]
 
     @classmethod
     async def get_it(cls) -> list[str]:
-        """获取it数据"""
-        res = await AsyncHttpx.get(cls.it_url)
-        root = ET.fromstring(res.text)
-        titles = []
-        for item in root.findall("./channel/item"):
-            title_element = item.find("title")
-            if title_element is not None:
-                titles.append(title_element.text)
-        return titles[:11] if len(titles) > 11 else titles
+        """获取IT资讯"""
+        try:
+            res = await AsyncHttpx.get(cls.it_url)
+            root = ET.fromstring(res.text)
+            titles = []
+            for item in root.findall("./channel/item"):
+                title_element = item.find("title")
+                if title_element is not None:
+                    titles.append(title_element.text)
+            return titles[:11] if len(titles) > 11 else titles
+        except Exception as e:
+            logger.error(f"获取IT资讯失败: {e}")
+            return ["获取IT资讯失败 QAQ"]
 
     @classmethod
     async def get_anime(cls) -> list[tuple[str, str]]:
-        """获取动漫数据"""
-        res = await AsyncHttpx.get(cls.anime_url)
-        data_list = []
-        week = datetime.now().weekday()
+        """获取今日新番"""
         try:
-            anime = Anime(**res.json()[week])
-        except IndexError:
-            anime = Anime(**res.json()[-1])
-        data_list.extend(
-            (data.name_cn or data.name, data.image) for data in anime.items
-        )
-        return data_list[:8] if len(data_list) > 8 else data_list
+            res = await AsyncHttpx.get(cls.anime_url)
+            data_list = []
+            week = datetime.now().weekday()
+            try:
+                anime = Anime(**res.json()[week])
+            except IndexError:
+                anime = Anime(**res.json()[-1])
+            data_list.extend(
+                (data.name_cn or data.name, data.image) for data in anime.items
+            )
+            return data_list[:8] if len(data_list) > 8 else data_list
+        except Exception as e:
+            logger.error(f"获取今日新番失败: {e}")
+            return [("获取今日新番失败 QAQ", "")]
